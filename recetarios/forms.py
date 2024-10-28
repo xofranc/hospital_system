89a5bb8cd8fbacf0b2e6c9f895da8478e2f3ea5e
Camilin28 <berbeocamilo@gmail.com>
2024-10-28 13:11:59 -0500

    Actualizacion models y forms

--- recetarios/forms.py
+++ recetarios/forms.py
@@ -1,9 +1,42 @@
-# recetarios/forms.py
-
 from django import forms
-from .models import RecetaMedica
+from .models import Proveedor, Medicamento, Cliente, Venta
 
-class RecetaMedicaForm(forms.ModelForm):
+# Formulario basado en el modelo Proveedor
+class ProveedorForm(forms.ModelForm):
     class Meta:
-        model = RecetaMedica
-        fields = ['paciente', 'medicamentos', 'instrucciones']
+        # Especificamos el modelo y los campos a incluir en el formulario
+        model = Proveedor
+        fields = ['nombre', 'direccion', 'telefono', 'email']
+        widgets = {
+            # Configuramos el widget del campo 'direccion' para usar un Textarea
+            'direccion': forms.Textarea(attrs={'rows': 3}),
+        }
+
+# Formulario basado en el modelo Medicamento
+class MedicamentoForm(forms.ModelForm):
+    class Meta:
+        # Especificamos el modelo y los campos a incluir en el formulario
+        model = Medicamento
+        fields = ['nombre', 'descripcion', 'precio', 'cantidad_disponible', 'proveedor']
+        widgets = {
+            # Configuramos el widget del campo 'descripcion' para usar un Textarea
+            'descripcion': forms.Textarea(attrs={'rows': 3}),
+        }
+
+# Formulario basado en el modelo Cliente
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        # Especificamos el modelo y los campos a incluir en el formulario
+        model = Cliente
+        fields = ['nombre', 'direccion', 'telefono', 'email']
+        widgets = {
+            # Configuramos el widget del campo 'direccion' para usar un Textarea
+            'direccion': forms.Textarea(attrs={'rows': 3}),
+        }
+
+# Formulario basado en el modelo Venta
+class VentaForm(forms.ModelForm):
+    class Meta:
+        # Especificamos el modelo y los campos a incluir en el formulario
+        model = Venta
+        fields = ['cliente', 'medicamento', 'cantidad']
