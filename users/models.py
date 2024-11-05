7a4c2a33e28866696c8f1173545a52dd57f723a9
Rey Omar Esguerra Ramirez <reyomar066@gmail.com>
2024-11-04 21:19:03 -0500

    Add-DoctorDashboard

--- users/models.py
+++ users/models.py
@@ -4,7 +4,6 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
-
 class UserManager(BaseUserManager):
     """
     Manager for the user model, provides methods to create user and superuser
