cdf6bb027f022620673f96f826d619db431740bb
Santiago Vasquez Francoclear <santiagop200101@gmail.com>
2024-11-12 16:14:12 -0500

    creacion de la aplicacion historias

---
+++ historial/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from users.models import Pacientes, Doctors
+# from multiselectfield import MultiSelectField
+# Create your models here.
+
+
+class Historia_clinica(models.Model):
+    pass
+
+
