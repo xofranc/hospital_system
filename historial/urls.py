ec6c496fdbd9a833d6d2a56451adef82299fb782
Santiago Vasquez Francoclear <santiagop200101@gmail.com>
2024-11-18 14:05:57 -0500

    actualizaciones

--- historial/urls.py
+++ historial/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('nueva_historia/', views.crear_historia, name='crear_historia'),
-    path('ver_historia/', views.ver_historia_clinica, name='ver_historia')
-]
+    path('ver_historia/<int:historia_clinica_id>/', views.ver_historia_clinica, name='ver_historia_clinica'),
+    ]
--- historial/views.py
+++ historial/views.py
@@ -11,52 +11,43 @@ from django.contrib.auth import login
 
 @login_required
 def crear_historia(request):
-    if not hasattr(request.user, 'doctor'):
-        messages.error(request, 'acceso no autorizado, Solo doctores pueden registrar historias clinicas')
-        return render(request,'doctor/doctor_login.html', {'form_doctor': form})
+    # if not hasattr(request.user, 'doctor'):
+    #     messages.error(request, 'acceso no autorizado, Solo doctores pueden registrar historias clinicas')
+    #     return render(request,'doctor/doctor_login.html', {'form_doctor': form})
     
     if request.method == 'POST':
         form = HistoriaClinicaForm(request.POST)
         
         if form.is_valid():
             historia_clinica = form.save(commit=False)
-            historia_clinica.doctor = request.user.doctor
+            model_doctor = request.user.content_type.model_class()
+            doctor = model_doctor.objects.get(id=request.user.role_data_id)
+            historia_clinica.doctor = doctor
             historia_clinica.save()
             
             
             messages.success(request, 'Historia Clinica guardada con exito')
-            return redirect('ver_historia_clinica', historia_clinica.id)  # Redirige a la vista de la historia clínica registrada
+            return redirect('ver_historia_clinica', historia_clinica_id=historia_clinica.id)
         else:
             messages.error(request, "Por favor, corrige los errores del formulario.")
     else:
         # Si la vista es GET, prellenamos el formulario si es necesario
         form = HistoriaClinicaForm()
 
-    return render(request, 'historias_clinicas/registrar_historia_clinica.html', {'form': form})
+    return render(request, 'registrar_historia_clinica.html', {'form': form})
 
 @login_required
 def ver_historia_clinica(request, historia_clinica_id):
     # Verificar que el usuario logueado sea un doctor
-    if not hasattr(request.user, 'doctor'):
-        messages.error(request, "Acceso no autorizado. Solo los doctores pueden ver las historias clínicas.")
-        return redirect('login')
+    # if not hasattr(request.user, 'doctor'):
+    #     messages.error(request, "Acceso no autorizado. Solo los doctores pueden ver las historias clínicas.")
+    #     return redirect('doctores/login.html')
 
     # Obtiene la historia clínica para mostrarla después de guardarla
     historia_clinica = get_object_or_404(Historia_clinica, id=historia_clinica_id)
 
-    return render(request, 'historias_clinicas/ver_historia_clinica.html', {'historia_clinica': historia_clinica})
+    return redirect('ver_historia_clinica', historia_clinica_id=historia_clinica.id)
         
         
         
         
-@login_required
-def ver_historia_clinica(request, historia_clinica_id):
-    # Verificar que el usuario logueado sea un doctor
-    if not hasattr(request.user, 'doctor'):
-        messages.error(request, "Acceso no autorizado. Solo los doctores pueden ver las historias clínicas.")
-        return redirect('login')
-
-    # Obtiene la historia clínica para mostrarla después de guardarla
-    historia_clinica = get_object_or_404(Historia_clinica, id=historia_clinica_id)
-
-    return render(request, 'historias_clinicas/ver_historia_clinica.html', {'historia_clinica': historia_clinica}) 
