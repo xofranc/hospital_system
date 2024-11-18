73f28138cd2b84c9eafad69a60631bd866c81c77
Santiago Vasquez Francoclear <santiagop200101@gmail.com>
2024-11-18 14:54:36 -0500

    arreglo logins

--- users/models.py
+++ users/models.py
@@ -45,7 +45,7 @@ class UserManager(BaseUserManager):
     def pacientes(self,base_data, role_data):
         base_data.setdefault("is_staff", False)
         base_data.setdefault("is_superuser", False)
-        base_data.setdefault("is_active", False)
+        base_data.setdefault("is_active", True)
         return self._create_user(
             related_model_name='pacientes',
             role_data=role_data,
